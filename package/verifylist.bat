66e3a1990c0abf81cdcb97373b928c53338c853c
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2024-10-13 21:21:17 +0800

    so new

--- package/verifylist.bat
+++ package/verifylist.bat
@@ -1,4 +1,5 @@
-set vldate=241012
-:: Updated at 2024/10/12 ::
+set vldate=241013
+:: Updated at 2024/10/13 ::
 if exist %root%\system\packages\com.skholdev.test echo if "%%command%%" == "test" (call packages\com.skholdev.test\test.bat) >> %root%\etc\packages.bat & echo echo test >> packages\list.bat
 if exist %root%\system\packages\com.skholdev.sakosp echo if "%%command%%" == "sakosp" (call packages\com.skholdev.sakosp\app.bat) >> %root%\etc\packages.bat & echo echo sakosp >> packages\list.bat
+if exist %root%\system\packages\com.lvzbx.pkg echo if "%%command%%" == "pkg" (call packages\com.lvzbx.pkg\pkg.bat) >> %root%\etc\packages.bat & echo echo pkg (lvzbx) >> packages\list.bat
