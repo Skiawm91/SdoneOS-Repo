d201de6fd8f03aadc78450fea0e26cd76ad4363c
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2024-10-24 22:14:55 +0800

    Update verifylist.bat

--- package/verifylist.bat
+++ package/verifylist.bat
@@ -1,5 +1,6 @@
-set vldate=241013
-:: Updated at 2024/10/13 ::
+set vldate=241024
+:: Updated at 2024/10/24 ::
 if exist %root%\system\packages\com.skholdev.test echo if "%%command%%" == "test" (call packages\com.skholdev.test\test.bat) >> %root%\etc\packages.bat & echo echo test >> packages\list.bat
 if exist %root%\system\packages\com.skholdev.sakosp echo if "%%command%%" == "sakosp" (call packages\com.skholdev.sakosp\app.bat) >> %root%\etc\packages.bat & echo echo sakosp >> packages\list.bat
 if exist %root%\system\packages\com.lvzbx.pkg echo if "%%command%%" == "pkg" (call packages\com.lvzbx.pkg\pkg.bat) >> %root%\etc\packages.bat & echo echo pkg (lvzbx) >> packages\list.bat
+if exist %root%\system\packages\com.ahqiang.hacknet echo if "%%command%%" == "hacknet" (call packages\com.ahqiang.hacknet\home.bat) >> %root%\etc\packages.bat & echo echo hacknet (ahqiang) >> packages\list.bat
