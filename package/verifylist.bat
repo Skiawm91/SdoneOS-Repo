d21fda138e2a5b9bf3f28d56dbc13b0fe6004131
Skiawm91 <96339124+Skiawm91@users.noreply.github.com>
2025-01-12 16:38:56 +0800

    Update verifylist.bat

--- package/verifylist.bat
+++ package/verifylist.bat
@@ -4,4 +4,4 @@ if exist %root%\system\packages\com.skholdev.test echo if "%%command%%" == "test
 if exist %root%\system\packages\com.skholdev.sakosp echo if "%%command%%" == "sakosp" (call packages\com.skholdev.sakosp\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo sakosp >> packages\list.bat
 if exist %root%\system\packages\com.lvzbx.pkg echo if "%%command%%" == "pkg" (call packages\com.lvzbx.pkg\pkg.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo pkg (lvzbx) >> packages\list.bat
 if exist %root%\system\packages\com.ahqiang.hacknet echo if "%%command%%" == "hacknet" (call packages\com.ahqiang.hacknet\home.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo hacknet (ahqiang) >> packages\list.bat
-if exist %root%\system\packages\com.skholdev.syscustom echo if "%%command%%" == "syscustom" (call packages\com.skholdev.syscustom\app.bat %%prefix%%) >> %root%\etc\packages.bat & echo echo syscustom >> packages\list.bat
+if exist %root%\system\packages\com.skholdev.syscustom echo if "%%command%%" == "syscustom" (call packages\com.skholdev.syscustom\app.bat %%%prefix%%%) >> %root%\etc\packages.bat & echo echo syscustom >> packages\list.bat
